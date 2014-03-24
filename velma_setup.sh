9d812f30997d3ffd1bf28ed605fe811891aa72bc
Konrad Banachowicz <konradb3@gmail.com>
2014-03-24 14:47:36 +0100

    ...

--- velma_setup.sh
+++ velma_setup.sh
@@ -2,15 +2,19 @@
 
 export OROCOS_TARGET=xenomai
 
-mkdir ws_velma
-cd ws_velma
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma.rosinstall -O /tmp/velma.rosinstall
+
+if [ ! -d $1 ]; then
+  mkdir $1
+fi
+cd $1
 wstool init
-wstool merge ../velma_sim.rosinstall
+wstool merge /tmp/velma.rosinstall
 wstool update
 cp ../friComm.h underlay/src/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/
 cd underlay_isolated
-catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB
+catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
 source install_isolated/setup.bash
 cd ../underlay
-catkin_make
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo
 source devel/setup.bash
