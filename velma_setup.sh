bc8929b8bffe3c62c12cea04bc0285a3272bdcd2
Konrad Banachowicz <konradb3@gmail.com>
2014-03-31 16:24:11 +0200

    ...

--- velma_setup.sh
+++ velma_setup.sh
@@ -8,11 +8,14 @@ wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/vel
 if [ ! -d $1 ]; then
   mkdir $1
 fi
+
+FRI_DIR=`pwd`
+
 cd $1
 wstool init
 wstool merge /tmp/velma.rosinstall
 wstool update
-cp ../friComm.h underlay/src/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/
+cp $FRI_DIR/friComm.h underlay/src/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/
 cd underlay_isolated
 catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
 source install_isolated/setup.bash
