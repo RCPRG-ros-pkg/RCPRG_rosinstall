a8bed9566e9a0211d165f2ec2eeb8fb8a659210f
Konrad Banachowicz <konradb3@gmail.com>
2014-04-05 09:35:54 +0200

    add irp6 rosinstall

---
+++ irp6_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/irp6.rosinstall -O /tmp/velma.rosinstall
+
+if [ ! -d $1 ]; then
+  mkdir $1
+fi
+
+cd $1
+wstool init
+wstool merge /tmp/irp6.rosinstall
+wstool update
+cd underlay_isolated
+catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
+source install_isolated/setup.bash
+cd ../underlay
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo
+source devel/setup.bash
