cb3240b0ada1082c489d297e0d86331d8332e795
Konrad Banachowicz <konradb3@gmail.com>
2014-05-06 10:13:15 +0200

    ..

---
+++ velma_setup_sim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+export OROCOS_TARGET=gnulinux
+
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_sim.rosinstall -O /tmp/velma_sim.rosinstall
+
+if [ ! -d $1 ]; then
+  mkdir $1
+fi
+
+FRI_DIR=`pwd`
+
+cd $1
+wstool init
+wstool merge /tmp/velma_sim.rosinstall
+wstool update
+cd underlay_isolated
+catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
+source install_isolated/setup.bash
+cd ../underlay
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo
+source devel/setup.bash
