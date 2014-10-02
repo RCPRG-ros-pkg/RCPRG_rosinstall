57ac31bcef92c9b9fd8c94f9c265a73fe357e027
twiniars <tomrobotics@gmail.com>
2014-10-02 10:30:15 +0200

    Update velma_setup.sh

--- velma_setup.sh
+++ velma_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-export XENOMAI_ROOT_DIR=/opt/xenomai_sorter
-export PATH=/opt/xenomai_sorter/bin/:$PATH
+export XENOMAI_ROOT_DIR=/opt/xenomai
+export PATH=/opt/xenomai/bin/:$PATH
 export LANG=en_US.UTF-8
 export OROCOS_TARGET=xenomai
 
