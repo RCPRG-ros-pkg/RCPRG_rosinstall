948778c9b224814c2d3ce2e0d2ceb8fed1c12b4f
Dawid Seredyński <dawid.seredynski@gmail.com>
2015-07-24 13:13:41 +0200

    added install for simulated velma kinematics workspace

---
+++ velma_sim_kinematics_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+export LANG=en_US.UTF-8
+export OROCOS_TARGET=gnulinux
+
+function usage {
+  echo "usage: $0 directory"
+}
+
+if [ $# -ne 1 ]; then 
+  usage
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  usage
+  exit 1
+fi
+
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_sim_kinematics.rosinstall -O /tmp/velma_sim_kinematics.rosinstall
+
+if [ ! -d $1 ]; then
+  mkdir $1
+fi
+
+FRI_DIR=`pwd`
+
+cd $1
+wstool init
+wstool merge /tmp/velma.rosinstall
+wstool update
+cd underlay_isolated
+catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
+source install_isolated/setup.bash
+cd ../underlay
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
+source devel/setup.bash
