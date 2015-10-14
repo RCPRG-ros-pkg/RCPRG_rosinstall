2a189da78dc2643a95d51aee318166c5eeb23949
Konrad Banachowicz <konradb3@gmail.com>
2015-10-14 14:39:16 +0200

    fix

--- velma_setup.sh
+++ velma_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 export XENOMAI_ROOT_DIR=/opt/xenomai
 export PATH=/opt/xenomai/bin/:$PATH
 export LANG=en_US.UTF-8
@@ -21,7 +20,7 @@ wstool update
 cp $FRI_DIR/friComm.h underlay/src/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/
 cd underlay_isolated
 catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
-source install_isolated/setup.bash
 cd ../underlay
-catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
+catkin config --extend $1/underlay_isolated/install_isolated/ --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
+catkin build
 source devel/setup.bash
