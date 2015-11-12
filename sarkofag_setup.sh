601c21767f799ef10190219f0ca4407f6dfaed75
Tomasz Winiarski <tomrobotics@gmail.com>
2015-11-12 13:15:00 +0100

    sarkofag temporarary files added

---
+++ sarkofag_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+export LANGUAGE=en
+
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/irp6.rosinstall -O /tmp/irp6.rosinstall
+
+if [ ! -d $1 ]; then
+  mkdir $1
+fi
+
+cd $1
+wstool init
+wstool merge /tmp/irp6.rosinstall
+wstool update
+cd underlay_isolated
+catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
+source install_isolated/setup.bash
+cd ../underlay
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
+source devel/setup.bash
