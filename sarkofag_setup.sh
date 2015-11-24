6b7ae233f82a2dfcdd9d35cc2339343ae8e000a0
bartswis <bartswis@gmail.com>
2015-11-24 16:23:11 +0100

    sarkofag_setup fix

--- sarkofag_setup.sh
+++ sarkofag_setup.sh
@@ -17,5 +17,5 @@ cd underlay_isolated
 catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
 source install_isolated/setup.bash
 cd ../underlay
-catkin_make_isolated -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
+catkin_make_isolated -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF --install
 source devel/setup.bash
