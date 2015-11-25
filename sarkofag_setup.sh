51dfe1e24698fe13bbde25335658e39a9b0b10e5
bartswis <bartswis@gmail.com>
2015-11-25 13:24:08 +0100

    sarkofag: new robot workspace

--- sarkofag_setup.sh
+++ sarkofag_setup.sh
@@ -18,4 +18,7 @@ catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB 
 source install_isolated/setup.bash
 cd ../underlay
 catkin_make_isolated -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF --install
-source devel/setup.bash
+source install_isolated/setup.bash
+cd ../robot
+catkin_make_isolated -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
+source devel_isolated/setup.bash
