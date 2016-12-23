e48f79b1bae1de6f6b0654718d743e3003a2a5d8
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-12-23 16:24:55 +0100

    workspace for barretthand grasp learning in simulation

--- barretthand_sim_setup.sh
+++ barretthand_sim_setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export LANG=en_US.UTF-8
-export OROCOS_TARGET=gnulinux
-
 
 function usage {
   echo "usage: $0 directory"
@@ -19,6 +17,7 @@ if [ -z "$1" ]; then
 fi
 
 wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/barretthand_sim.rosinstall -O /tmp/barretthand_sim.rosinstall
+#cp ~/code/RCPRG_rosinstall/barretthand_sim.rosinstall /tmp/barretthand_sim.rosinstall
 
 if [ ! -d $1 ]; then
   mkdir $1
@@ -28,8 +27,12 @@ cd $1
 wstool init
 wstool merge /tmp/barretthand_sim.rosinstall
 wstool update
-cd underlay_isolated
-catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cd ../underlay
-catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
-source devel/setup.bash
+
+cd underlay
+catkin config --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=ON   -DUSE_DOUBLE_PRECISION=ON
+catkin build
+
+cd ../sim
+catkin config --extend ../underlay/devel/ --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+catkin build
+
