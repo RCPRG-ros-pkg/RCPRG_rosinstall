54036c9173ee0511a3d22a20bc62b6c09ed3e3e0
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-04-20 17:15:27 +0200

    velma setup

--- velma_setup.sh
+++ velma_setup.sh
@@ -5,7 +5,7 @@ export LANG=en_US.UTF-8
 
 # useful functions
 function usage {
-  echo "usage: $0 directory [-xenomai]"
+  echo "usage: $0 directory"
 }
 
 function printError {
@@ -14,7 +14,7 @@ function printError {
     echo -e "${RED}$1${NC}"
 }
 
-if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   usage
   exit 1
 fi
@@ -22,17 +22,6 @@ fi
 if [ -z "$1" ]; then
   usage
   exit 1
-fi
-
-if [ $# -eq 2 ] && [ "$2" != "-xenomai" ]; then
-  usage
-  exit 1
-fi
-
-if [ $# -eq 2 ] && [ "$2" == "-xenomai" ]; then
-  use_xenomai=true
-else
-  use_xenomai=false
 fi
 
 distro="$ROS_DISTRO"
@@ -69,6 +58,8 @@ installed=(
 "ros-$distro-controller-manager"
 "ros-$distro-urdfdom-py"
 "ros-$distro-transmission-interface"
+"ros-$distro-eigen-conversions"
+"ros-$distro-octomap"
 )
 
 # the list of packages that should be uninstalled
@@ -128,20 +119,19 @@ if [ "$error" = true ]; then
 fi
 
 # test
-cp common_orocos.rosinstall       /tmp/common_orocos.rosinstall
-cp common_velma.rosinstall        /tmp/common_velma.rosinstall
-cp gazebo7_2_dart.rosinstall      /tmp/gazebo7_2_dart.rosinstall
-cp velma_sim.rosinstall           /tmp/velma_sim.rosinstall
-cp velma_applications.rosinstall  /tmp/velma_applications.rosinstall
-if [ "$use_xenomai" = true ]; then
-    cp velma_hw.rosinstall        /tmp/velma_hw.rosinstall
-fi
+#cp common_orocos.rosinstall       /tmp/common_orocos.rosinstall
+#cp common_velma.rosinstall        /tmp/common_velma.rosinstall
+#cp gazebo7_2_dart.rosinstall      /tmp/gazebo7_2_dart.rosinstall
+#cp velma_sim.rosinstall           /tmp/velma_sim.rosinstall
+#cp velma_applications.rosinstall  /tmp/velma_applications.rosinstall
+#cp velma_hw.rosinstall            /tmp/velma_hw.rosinstall
 
-#wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_orocos.rosinstall       -O /tmp/common_orocos.rosinstall
-#wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_velma.rosinstall        -O /tmp/common_velma.rosinstall
-#wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/gazebo7_2_dart.rosinstall      -O /tmp/gazebo7_2_dart.rosinstall
-#wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_sim.rosinstall           -O /tmp/velma_sim.rosinstall
-#wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_applications.rosinstall  -O /tmp/velma_applications.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_orocos.rosinstall       -O /tmp/common_orocos.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_velma.rosinstall        -O /tmp/common_velma.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/gazebo7_2_dart.rosinstall      -O /tmp/gazebo7_2_dart.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_sim.rosinstall           -O /tmp/velma_sim.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_applications.rosinstall  -O /tmp/velma_applications.rosinstall
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_hw.rosinstall            -O /tmp/velma_hw.rosinstall
 
 build_dir=$1
 
@@ -155,12 +145,6 @@ cd $build_dir
 
 WORKSPACE_ROOT_DIR=`pwd`
 
-#if [ ! -d sources ]; then
-mkdir -p sources
-#fi
-
-cd sources
-
 if [ ! -e ".rosinstall" ]; then
   wstool init
 fi
@@ -170,21 +154,19 @@ wstool merge /tmp/common_velma.rosinstall
 wstool merge /tmp/gazebo7_2_dart.rosinstall
 wstool merge /tmp/velma_sim.rosinstall
 wstool merge /tmp/velma_applications.rosinstall
-if [ "$use_xenomai" = true ]; then
-    wstool merge /tmp/velma_hw.rosinstall
-fi
+wstool merge /tmp/velma_hw.rosinstall
 
 #TODO: uncomment
-#wstool update
+wstool update
 
 # download package.xml for some packages
-wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_dart-core.xml -O sim/underlay_isolated/gazebo/dart/package.xml
-wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_sdformat.xml  -O sim/underlay_isolated/gazebo/sdformat/package.xml
-wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_gazebo.xml    -O sim/underlay_isolated/gazebo/gazebo/package.xml
-wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_ign-math.xml  -O sim/underlay_isolated/gazebo/ign-math/package.xml
+wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_dart-core.xml -O underlay_isolated/src/gazebo/dart/package.xml
+wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_sdformat.xml  -O underlay_isolated/src/gazebo/sdformat/package.xml
+wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_gazebo.xml    -O underlay_isolated/src/gazebo/gazebo/package.xml
+wget https://bitbucket.org/scpeters/unix-stuff/raw/master/package_xml/package_ign-math.xml  -O underlay_isolated/src/gazebo/ign-math/package.xml
 
 # copy friComm.h
-cp -f $FRI_DIR/friComm.h common/underlay/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/
+cp -f $FRI_DIR/friComm.h underlay/src/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/
 if [ $? -eq 0 ]; then
     echo "cp friComm.h OK"
 else
@@ -193,151 +175,9 @@ else
 fi
 
 #
-# create workspace directory tree
-#
-cd $WORKSPACE_ROOT_DIR
-
-mkdir -p sim/underlay_isolated/src
-mkdir -p sim/underlay/src
-mkdir -p sim/top/src
-
-mkdir -p hw/underlay_isolated/src
-mkdir -p hw/underlay/src
-mkdir -p hw/top/src
-
-# common
-if [ -d sources/common/underlay_isolated ]; then
-    common_repos_underlay_isolated=($(ls sources/common/underlay_isolated))
-else
-    common_repos_underlay_isolated=()
-fi
-
-if [ -d sources/common/underlay ]; then
-    common_repos_underlay=($(ls sources/common/underlay))
-else
-    common_repos_underlay=()
-fi
-
-if [ -d sources/common/top ]; then
-    common_repos_top=($(ls sources/common/top))
-else
-    common_repos_top=()
-fi
-
-# hw
-if [ -d sources/hw/underlay_isolated ]; then
-    hw_repos_underlay_isolated=($(ls sources/hw/underlay_isolated))
-else
-    hw_repos_underlay_isolated=()
-fi
-
-if [ -d sources/hw/underlay ]; then
-    hw_repos_underlay=($(ls sources/hw/underlay))
-else
-    hw_repos_underlay=()
-fi
-
-if [ -d sources/hw/top ]; then
-    hw_repos_top=($(ls sources/hw/top))
-else
-    hw_repos_top=()
-fi
-
-# sim
-if [ -d sources/sim/underlay_isolated ]; then
-    sim_repos_underlay_isolated=($(ls sources/sim/underlay_isolated))
-else
-    sim_repos_underlay_isolated=()
-fi
-
-if [ -d sources/sim/underlay ]; then
-    sim_repos_underlay=($(ls sources/sim/underlay))
-else
-    sim_repos_underlay=()
-fi
-
-if [ -d sources/sim/top ]; then
-    sim_repos_top=($(ls sources/sim/top))
-else
-    sim_repos_top=()
-fi
-
-#
-# create symbolic links
-#
-function tryCreateLink {
-    if [ ! -h $2 ]; then
-        ln --symbolic --relative --no-dereference $1 $2
-    fi
-}
-
-# common
-for d in "${common_repos_underlay_isolated[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/common/underlay_isolated/$d  $WORKSPACE_ROOT_DIR/hw/underlay_isolated/src/$d
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/common/underlay_isolated/$d  $WORKSPACE_ROOT_DIR/sim/underlay_isolated/src/$d
-done
-
-for d in "${common_repos_underlay[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/common/underlay/$d  $WORKSPACE_ROOT_DIR/hw/underlay/src/$d
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/common/underlay/$d  $WORKSPACE_ROOT_DIR/sim/underlay/src/$d
-done
-
-for d in "${common_repos_top[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/common/top/$d  $WORKSPACE_ROOT_DIR/hw/top/src/$d
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/common/top/$d  $WORKSPACE_ROOT_DIR/sim/top/src/$d
-done
-
-# sim
-for d in "${sim_repos_underlay_isolated[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/sim/underlay_isolated/$d  $WORKSPACE_ROOT_DIR/sim/underlay_isolated/src/$d
-done
-
-for d in "${sim_repos_underlay[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/sim/underlay/$d  $WORKSPACE_ROOT_DIR/sim/underlay/src/$d
-done
-
-for d in "${sim_repos_top[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/sim/top/$d  $WORKSPACE_ROOT_DIR/sim/top/src/$d
-done
-
-# hw
-for d in "${hw_repos_underlay_isolated[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/hw/underlay_isolated/$d  $WORKSPACE_ROOT_DIR/hw/underlay_isolated/src/$d
-done
-
-for d in "${hw_repos_underlay[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/hw/underlay/$d  $WORKSPACE_ROOT_DIR/hw/underlay/src/$d
-done
-
-for d in "${hw_repos_top[@]}"
-do
-    tryCreateLink $WORKSPACE_ROOT_DIR/sources/hw/top/$d  $WORKSPACE_ROOT_DIR/hw/top/src/$d
-done
-
-#
 # underlay_isolated
 #
-cd $WORKSPACE_ROOT_DIR/hw/underlay_isolated
-export OROCOS_TARGET=xenomai
-catkin config --cmake-args -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_CORE_ONLY=ON   -DBUILD_SHARED_LIBS=ON   -DUSE_DOUBLE_PRECISION=ON -DBUILD_HELLOWORLD=OFF
-catkin build
-if [ $? -eq 0 ]; then
-    echo "underlay_isolated build OK"
-else
-    printError "underlay_isolated build FAILED"
-    exit 1
-fi
-
-cd $WORKSPACE_ROOT_DIR/sim/underlay_isolated
-export OROCOS_TARGET=gnulinux
+cd $WORKSPACE_ROOT_DIR/underlay_isolated
 catkin config --cmake-args -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_CORE_ONLY=ON   -DBUILD_SHARED_LIBS=ON   -DUSE_DOUBLE_PRECISION=ON -DBUILD_HELLOWORLD=OFF
 catkin build
 if [ $? -eq 0 ]; then
@@ -350,19 +190,7 @@ fi
 #
 # underlay
 #
-cd $WORKSPACE_ROOT_DIR/hw/underlay
-export OROCOS_TARGET=xenomai
-catkin config --extend ../underlay_isolated/devel/ --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
-catkin build
-if [ $? -eq 0 ]; then
-    echo "underlay build OK"
-else
-    printError "underlay build FAILED"
-    exit 1
-fi
-
-cd $WORKSPACE_ROOT_DIR/sim/underlay
-export OROCOS_TARGET=gnulinux
+cd $WORKSPACE_ROOT_DIR/underlay
 catkin config --extend ../underlay_isolated/devel/ --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
 catkin build
 if [ $? -eq 0 ]; then
@@ -375,14 +203,7 @@ fi
 #
 # top
 #
-cd $WORKSPACE_ROOT_DIR/hw/top
-export OROCOS_TARGET=xenomai
-catkin config --extend ../underlay/devel/ --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
-catkin build
-source devel/setup.bash
-
-cd $WORKSPACE_ROOT_DIR/sim/top
-export OROCOS_TARGET=gnulinux
+cd $WORKSPACE_ROOT_DIR/top
 catkin config --extend ../underlay/devel/ --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=OFF
 catkin build
 source devel/setup.bash
