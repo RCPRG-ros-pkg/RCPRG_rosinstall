2906459eaac259fe8f1f34fefc083d693b7d3ce5
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-06-23 17:39:24 +0200

    dir scan support in version generator

--- make_version_snapshot.py
+++ make_version_snapshot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys, getopt, os, subprocess
 import math
 import copy
@@ -10,8 +11,84 @@ class Repository:
         self.uri = None
         self.commit_sha = None
 
+def isGitRepo(path):
+    with open(os.devnull, 'w') as FNULL:
+        is_repo = (subprocess.call(['git', 'status'], stdout=FNULL, stderr=FNULL, cwd=path) == 0)
+    return is_repo
+
+def getGitBranch(path):
+    out_read, out_write = os.pipe()
+    subprocess.call(['git', 'rev-parse', "--abbrev-ref", 'HEAD'], stdout=out_write, cwd=path)
+    branch_str = os.read(out_read, 1000)
+    os.close(out_read)
+    return branch_str
+
+def getGitRemoteUrl(path):
+    out_read, out_write = os.pipe()
+    subprocess.call(['git', 'remote', "get-url", 'origin'], stdout=out_write, cwd=path)
+    url_str = os.read(out_read, 1000)
+    os.close(out_read)
+    return url_str
+
+def getGitChanges(path):
+    out_read, out_write = os.pipe()
+    subprocess.call(['git', 'status'], stdout=out_write, cwd=path)
+    status_str = os.read(out_read, 1000)
+    os.close(out_read)
+    if status_str.strip().endswith("nothing to commit, working directory clean"):
+        status_str = None
+
+    return status_str
+
+    print "getGitChanges 1"
+    out_read, out_write = os.pipe()
+    subprocess.call(['git', 'diff-index', "HEAD", '--'], stdout=out_write, cwd=path)
+    diff_str = os.read(out_read, 1000)
+    os.close(out_read)
+
+    print "getGitChanges 2"
+    out_read, out_write = os.pipe()
+    subprocess.call(['git', 'ls-files', "--others", '--exclude-standard'], stdout=out_write, cwd=path)
+    untracked_str = os.read(out_read, 1000)
+    os.close(out_read)
+    print "getGitChanges 3"
+
+    if len(diff_str) == 0:
+        diff_str = None
+    if len(untracked_str) == 0:
+        untracked_str = None
+    return (diff_str, untracked_str)
+
+def isHgRepo(path):
+    with open(os.devnull, 'w') as FNULL:
+        is_repo = (subprocess.call(['hg', 'status'], stdout=FNULL, stderr=FNULL, cwd=path) == 0)
+    return is_repo
+
+def getHgTag(path):
+    out_read, out_write = os.pipe()
+    subprocess.call(['hg', 'id', "-t"], stdout=out_write, cwd=path)
+    branch_str = os.read(out_read, 1000)
+    os.close(out_read)
+    return branch_str
+
+def getHgUrl(path):
+    out_read, out_write = os.pipe()
+    subprocess.call(['hg', 'paths', "default"], stdout=out_write, cwd=path)
+    url_str = os.read(out_read, 1000)
+    os.close(out_read)
+    return url_str
+
+def recursiveFindRepositories(path, repo_list):
+    if isGitRepo(path) or isHgRepo(path):
+        repo_list.append(path)
+        return
+
+    subdirs = next(os.walk(path))[1]
+    for d in subdirs:
+        recursiveFindRepositories(path + "/" + d, repo_list)
+
 def main(argv):
-    usage_str = 'test.py -i <rosinstallfile>'
+    usage_str = 'test.py [-i <rosinstallfile>]'
     rosinstallfile = None
     try:
         opts, args = getopt.getopt(argv,"hi:",["rosinstallfile="])
@@ -26,8 +103,41 @@ def main(argv):
             rosinstallfile = arg
 
     if not rosinstallfile:
-        print usage_str
-        sys.exit(1)
+        # scan subdirectories
+        repo_list = []
+        dirs = next(os.walk("."))[1]
+        for d in dirs:
+            subdirs = next(os.walk("./" + d))[1]
+            if "src" in subdirs:
+                recursiveFindRepositories("./" + d + "/src", repo_list)
+
+        changes_list = {}
+
+        for path in repo_list:
+            if isGitRepo(path):
+                repo = "git"
+                uri = getGitRemoteUrl(path)
+                version = getGitBranch(path)
+                changes = getGitChanges(path)
+            elif isHgRepo(path):
+                repo = "hg"
+                uri = getHgUrl(path)
+                version = getHgTag(path)
+                changes = None  # TODO
+            else:
+                raise
+            if path.startswith("./"):
+                p = path[2:]
+            else:
+                p = path
+            print "- %s: {local-name: %s, uri: '%s', version: '%s'}"%(repo, p, uri.strip(), version.strip())
+            if changes:
+                changes_list[path] = changes
+
+        print "# some packages contain local changes:"
+        for ch in changes_list:
+            print "#", ch
+        exit(0)
 
     rosinstall_full_path = None
     repos = []
