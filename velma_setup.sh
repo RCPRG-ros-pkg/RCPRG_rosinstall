8e32ee08d9433817c7d41c774b4a4ccbb0634cb9
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-07-04 13:13:48 +0200

    new version for velma

--- velma_setup.sh
+++ velma_setup.sh
@@ -155,35 +155,30 @@ fi
 #cp common_orocos.rosinstall       /tmp/common_orocos.rosinstall
 #cp common_velma.rosinstall        /tmp/common_velma.rosinstall
 #cp gazebo7_2_dart.rosinstall      /tmp/gazebo7_2_dart.rosinstall
-#cp velma_sim.rosinstall           /tmp/velma_sim.rosinstall
-#cp velma_applications.rosinstall  /tmp/velma_applications.rosinstall
+#cp common_agent.rosinstall        /tmp/common_agent.rosinstall
 #cp velma_hw.rosinstall            /tmp/velma_hw.rosinstall
 
 if [ "$version" == "latest" ]; then
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_orocos.rosinstall       -O /tmp/common_orocos.rosinstall
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_velma.rosinstall        -O /tmp/common_velma.rosinstall
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/gazebo7_2_dart.rosinstall      -O /tmp/gazebo7_2_dart.rosinstall
-    wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_sim.rosinstall           -O /tmp/velma_sim.rosinstall
-    wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_applications.rosinstall  -O /tmp/velma_applications.rosinstall
+    wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_agent.rosinstall        -O /tmp/common_agent.rosinstall
 
     wstool merge /tmp/common_orocos.rosinstall
     wstool merge /tmp/common_velma.rosinstall
     wstool merge /tmp/gazebo7_2_dart.rosinstall
-    wstool merge /tmp/velma_sim.rosinstall
-    wstool merge /tmp/velma_applications.rosinstall
+    wstool merge /tmp/common_agent.rosinstall
 elif [ "$version" == "latest_hw" ]; then
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_orocos.rosinstall       -O /tmp/common_orocos.rosinstall
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_velma.rosinstall        -O /tmp/common_velma.rosinstall
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/gazebo7_2_dart.rosinstall      -O /tmp/gazebo7_2_dart.rosinstall
-    wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_sim.rosinstall           -O /tmp/velma_sim.rosinstall
-    wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_applications.rosinstall  -O /tmp/velma_applications.rosinstall
+    wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/common_agent.rosinstall        -O /tmp/common_agent.rosinstall
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma_hw.rosinstall            -O /tmp/velma_hw.rosinstall
 
     wstool merge /tmp/common_orocos.rosinstall
     wstool merge /tmp/common_velma.rosinstall
     wstool merge /tmp/gazebo7_2_dart.rosinstall
-    wstool merge /tmp/velma_sim.rosinstall
-    wstool merge /tmp/velma_applications.rosinstall
+    wstool merge /tmp/common_agent.rosinstall
     wstool merge /tmp/velma_hw.rosinstall
 else
     wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/velma/$version       -O /tmp/velma.rosinstall
