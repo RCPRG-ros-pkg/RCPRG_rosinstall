3f0500f10fb8ca35a158bcc89cc9edfe43da3b64
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-10-03 12:09:56 +0200

    wstool init first time only

--- barretthand_sim_setup.sh
+++ barretthand_sim_setup.sh
@@ -19,16 +19,21 @@ fi
 wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/barretthand_sim.rosinstall -O /tmp/barretthand_sim.rosinstall
 #cp ~/code/RCPRG_rosinstall/barretthand_sim.rosinstall /tmp/barretthand_sim.rosinstall
 
-if [ ! -d $1 ]; then
-  mkdir $1
+if [ ! -d "$1" ]; then
+  mkdir "$1"
 fi
 
-cd $1
-wstool init
+cd "$1"
+
+if [ ! -e ".rosinstall" ]; then
+  wstool init
+fi
+
 wstool merge /tmp/barretthand_sim.rosinstall
 wstool update
 
 cd underlay
+
 catkin config --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=ON   -DUSE_DOUBLE_PRECISION=ON
 catkin build
 
